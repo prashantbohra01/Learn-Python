ef28c4a56de85e3fdd9768c4078a11386966e17a
prashantbohra01 <prashantb4157@gmail.com>
2023-10-04 11:34:46 +0530

    comments and escape seq

---
+++ day2.py
@@ -0,0 +1,12 @@
+# Title: Comments, Escape sequences, Print statements
+
+print("My name is Prashant Bohra") #Print single line
+
+print("My name is Prashant\nand I am the author of New Book") #for multi-lines use \n
+
+# We can use #, ''', """ to comment out Multiple Lines"
+
+# we can use sep="~" to change the default seperator which is space
+
+print("hey", 6, 7, sep="~", end="009\n")
+print("Hello")
