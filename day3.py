97b85aaca80bc1330806fb7f5af60d98c45495fb
prashantbohra01 <prashantb4157@gmail.com>
2023-10-04 12:10:30 +0530

    variables and datatypes

---
+++ day3.py
@@ -0,0 +1,44 @@
+#Title: Variables and Data Types
+
+a=123
+b="Prashant"      
+c=True
+d=None
+
+print(a,b,c,d)
+
+print( "a is a", type(a))
+print( "b is a", type(b))
+print( "c is a", type(c))
+print( "d is a", type(d))
+
+# Data types specifies the type of values a variable holds. 
+# This is required in programming to do various operations without any error
+
+''' 1. NUMERIC DATA TYPE (int, float, complex)
+
+int = 3, -8, 0
+float = 1.2, -9.0, 0.00001
+complex = 6+2i
+'''
+
+''' 2. Text Data Types(string)
+   
+str: "Hello world"
+'''
+
+''' 3. Boolean Data Types
+It consists of values true and false
+'''
+
+'''4. Sequence Data Types(list and tuple)
+
+list: It is a ordered collection of data with elements seperated by commas and enclosed within sqaure brackets.
+List are mutable and can be modified after creation.
+
+Tuple: It is a ordered collection of data with elements seperated by commas and enclosed within parantheses.
+Tuples are immutable and can not be modified after creation.
+'''
+
+'''5. Mapped data (dict)
+'''
