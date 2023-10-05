6e08a3ff909b251d4a040d87e703f50ecc3cecb1
prashantbohra01 <prashantb4157@gmail.com>
2023-10-05 12:42:36 +0530

    arithmetic operations

---
+++ day4.py
@@ -0,0 +1,13 @@
+
+# Arithmetic operations
+
+print(15+6)  # Addition ( For addition of two numbers )
+print(15-6)  # Subtraction ( For subtraction of two numbers )
+
+print(15*6)  # Multiplication ( For multiplication of two numbers )
+print(15**3) # Exponential ( for eg. 15*15*15)
+
+print(15/6)  # Divide ( For division of two numbers )
+print(15//6) # Floor Division ( to get the division value without decimal )
+
+print(15%6)  # Modulo ( For modulo of two numbers )It gives the value of reminder which we get after division
