35f80d99d100b34456b03abec2c9fb52236c5203
prashantbohra01 <prashantb4157@gmail.com>
2023-10-05 13:03:08 +0530

    simple calculator

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+# Making an simple calculator 
+
+a= 40
+b= 32
+
+print("The value of", a, "+", b, "is:", a+b )
+print("The value of", a, "-", b, "is:", a-b )
+print("The value of", a, "*", b, "is:", a*b )
+print("The value of", a, "/", b, "is:", a/b )
+print("The value of", a, "%", b, "is:", a%b )
