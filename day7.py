4d8a24345fdaa9c7d2f0bbca07f880bae09bb367
prashantbohra01 <prashantb4157@gmail.com>
2023-10-06 22:15:02 +0530

    strings

---
+++ day7.py
@@ -0,0 +1,23 @@
+#_______________________Strings_________________________
+
+
+# Anything which you enlcose in single or double quotes will be considered as string.
+# A string is essentially a sequence or array of textual data.
+
+name="Prashant"   # string 
+
+print("My name is " + name)
+
+a='He is a "Good" boy' # we can use this to print double quotes without getting quotation error.
+print(a)
+
+print("_________________________________________________")
+
+# to use multiple line in a string we can enclose them in triple single or double quotes.
+b= '''Hello
+I am Prashant
+I like "Mangoes"
+I am a good person
+'''
+
+print(b)
