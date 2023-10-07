5f35f7a87c3cbe2b21b549aef350da1ab5cf1494
prashantbohra01 <prashantb4157@gmail.com>
2023-10-07 22:39:04 +0530

    String Methods

---
+++ day9.py
@@ -0,0 +1,12 @@
+#_______________String_Methods____________
+
+a="prashant!!!!"
+
+print(len (a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("prashant","Bohra"))
+print(a.split(" "))
+print(a.capitalize())
+
