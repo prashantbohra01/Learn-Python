ef9838447ac4336b83fa8dfede6f68d3acbf66e5
prashantbohra01 <prashantb4157@gmail.com>
2023-10-07 22:03:35 +0530

    String Slicing

---
+++ day8.py
@@ -0,0 +1,16 @@
+#______________STRING_SLICING______________________
+
+name="Prashant"
+print(name[1])
+ 
+ # We can use this method to print a specific string value, 
+ # String value always starts from 0 to n-1.
+
+
+fruit="Mango"
+print(fruit[0:4]) # We can use this method to print from a specific string value to a different value.
+
+print(len(fruit)) 
+print(len(name))  # We can use this method to print the length of whole string.
+
+print(name[1:-4])
