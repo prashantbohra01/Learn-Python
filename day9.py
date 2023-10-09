1ce49bc23ae9c4797716a3284dde1bb9b65b7340
prashantbohra01 <prashantb4157@gmail.com>
2023-10-09 21:20:57 +0530

    Strings

--- day9.py
+++ day9.py
@@ -3,10 +3,15 @@
 a="prashant!!!!"
 
 print(len (a))
-print(a.upper())
-print(a.lower())
-print(a.rstrip("!"))
-print(a.replace("prashant","Bohra"))
-print(a.split(" "))
-print(a.capitalize())
+
+print(a.upper()) # used to convert text to uppercase.
+print(a.lower()) # used to convert text to lowercase.
+
+print(a.rstrip("!")) # used to remove particular characters.
+print(a.replace("prashant","Bohra")) # used to replace characters.
+
+print(a.split(" ")) # used to split string in list
+print(a.capitalize()) # used to capitalize first letter of string in a paragraph.
+
+
 
