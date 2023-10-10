137ccbd230bf12c9ccdb367954726649363a93d6
prashantbohra01 <prashantb4157@gmail.com>
2023-10-10 10:45:04 +0530

    If else

---
+++ day10.py
@@ -0,0 +1,9 @@
+#_________IF_ELSE_CONDITIONAL_STATEMENT______________
+
+a=int(input("Enter your age: "))
+print("your age is: ", a)
+
+if(a>1918):
+    print("you can drive")
+else:
+    print("you can not drive")
