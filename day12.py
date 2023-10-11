0266b3fbcec9172d18bea5736d74fa49124f3783
prashantbohra01 <prashantb4157@gmail.com>
2023-10-11 22:00:03 +0530

    For loop

---
+++ day12.py
@@ -0,0 +1,23 @@
+#_______For_Loops________________
+
+# Example 1:
+a="Prashant"
+for i in a:  # for loop in string
+    print(i)
+
+#Example 2:
+color=["red", "green", "blue", "yellow"]
+
+for x in color:
+    print(x)
+                     # for loop used for Lists
+    for y in x:
+     print(y)
+
+#Example 3:
+
+for n in range(5):
+   print(n)
+
+for k in range(0,7):
+   print(k+1)
