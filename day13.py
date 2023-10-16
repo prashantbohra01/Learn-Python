5bbca66293ca3d0fede18c246c7b6f2b9e857e75
prashantbohra01 <prashantb4157@gmail.com>
2023-10-16 16:43:58 +0530

    while loop

---
+++ day13.py
@@ -0,0 +1,40 @@
+#____________While_Loop____________
+
+
+#Example 1
+i=0
+
+while(i<6):
+    print(i)
+    i=i+1
+
+
+#Example 2
+
+i=int(input("Enter a random number: "))
+print(i)
+
+while(i<38):
+   i=int(input("Enter a random number: "))
+   print(i)
+
+print("Loop ended successfully") 
+    
+
+#Example 3
+
+count = 5
+ 
+while(count>0):
+    print(count)
+    count=count-1
+
+else:
+    print("Inside else")
+
+
+'''
+   While Loop is used to execute a block of statements repeatedly until a 
+   given condition is satisfied. And when the condition becomes false, the 
+   line immediately after the loop in the program is executed.
+'''
