1dd48400ec575e3b4e6bdff184c3e5b7c9a2c073
prashantbohra01 <prashantb4157@gmail.com>
2023-10-27 16:54:45 +0530

    lists

--- day16.py
+++ day16.py
@@ -16,9 +16,20 @@ print(marks[len(marks)-3])
 if 6 in marks:
     print("yes")
 else:
-    print("no")
+    print("no")  #check whether an item is present in the list.
 
 if "Prashant" in marks:
     print("yes")
 else:
     print("no")
+
+
+'''
+   * Lists are used to store multiple items in a single variable.
+
+   *  Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are 
+    Tuple, Set, and Dictionary, all with different qualities and usage.
+
+   * Lists are created using square brackets[]:
+
+'''
