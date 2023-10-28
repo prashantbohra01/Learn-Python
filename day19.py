9c37e96689a2b6cb3e1d5158b48a7bf0d9eca23f
prashantbohra01 <prashantb4157@gmail.com>
2023-10-28 21:45:07 +0530

    F-string

---
+++ day19.py
@@ -0,0 +1,9 @@
+#_________F-String____________
+
+m="Hey I'm {} and I'm from {}."
+country="India"
+name="Prashant"
+
+print(m.format(name, country)) #old format
+
+print(f"Hey I'm {name} and I'm from {country}.")
