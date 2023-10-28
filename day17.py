e72bbd48a38aff3d96ad8bd7fb86fb8c4b1dc0ac
prashantbohra01 <prashantb4157@gmail.com>
2023-10-28 17:28:21 +0530

    Lists Methods

--- day17.py
+++ day17.py
@@ -1,14 +1,19 @@
 #___Lists_Methods___
 
 l= [0, 2, 5, 3, 6, 9]
-# print(l)
-# l.append(8) # to add a new item to the list.
-# print(l)
+print(l)
+l.append(8) # to add a new item to the list.
+print(l)
 
-# l.sort() # sort the list.
-# print(l)
-# l.sort(reverse=True) # sorted in descending order.
-# print(l)
+l.sort() # sort the list.
+print(l)
+l.sort(reverse=True) # sorted in descending order.
+print(l)
 
 l.reverse() # reverse the list
 print(l)
+
+
+#insert()
+#extend() this method adds an entire lists or any other collection datatype
+
