cf83b30791ca0aae41c49cd37a5903c4d39ad3c4
prashantbohra01 <prashantb4157@gmail.com>
2024-05-19 17:24:58 +0530

    Day27 Modules

---
+++ day27.py
@@ -0,0 +1,23 @@
+# Modules 
+# Modules are the (.py)files that contain set of function you want to include in your program
+
+# Some inbuilt modules in python
+# 1. DateTime
+# 2. Random
+# 3. Math
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+import random
+
+y = random.randint(0, 10)
+print(y)
+
+import math
+z = max(12,1,54)
+print(z)
+y = min(13,11,24)
+print(y)
