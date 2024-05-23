5234c2f5e73b0577aaabf843b083bb9405740880
prashantbohra01 <prashantb4157@gmail.com>
2024-05-23 21:48:31 +0530

    Day28 Numpy-Array

--- Numpy/day28.py (Numpy).py
+++ Numpy/day28.py (Numpy).py
@@ -26,7 +26,6 @@ print(a)  # output without any separation coma
 print(b)  # output without any separation coma 
 print(a+b)  # Addition of both Arrays. 
 
-
 # Slicing of array
 
 arr = np.array([10,20,30,40])
@@ -35,4 +34,21 @@ arr = np.array([10,20,30,40])
 # print(arr[0:3])
 
 arr1 = np.array([[10,20,30,40],[50,60,70,80]])
-print(arr1[0:2,1:4])
+print(arr1[0:2,1:4])   # giving equal rows because in numpy it show equal elements on both the array. 
+
+arr2 = np.array([[10,20,30,40],[50,60,70,80],[90,100,110,120]])
+print(arr2[0,1:3])  # so here 1st argument will be the index(0) and second will be the range
+print(arr2[2,0:4])  # so here 1st argument will be the index(2) and second will be the range
+
+
+# Attributes/Functions of Array
+
+arr3 = np.array([[10,20,30,40],[50,60,70,80],[90,100,110,120]])
+print(np.shape(arr3)) # This attribute shows that our matrix will be of 3 rows and 4 columns. 
+print(np.size(arr3))  # Number of array elements
+print(np.ndim(arr3)) # it give no. dimension of array which is 2 here rows and columns. 
+print(arr3.dtype) # Data type of array elements
+print(len(arr3))
+print(arr3.astype(float)) # to convert the data type into float, string etc. 
+
+
