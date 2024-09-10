5230764b1717ef0d06e3165c4265cae12e6ee074
Prashant Bohra <prashantbohra233@gmail.com>
2024-09-10 15:16:37 +0530

    Add files via upload

---
+++ Integers/08_primeNumber.py
@@ -0,0 +1,22 @@
+# Prime number
+# Prime number is a number that is greater than 1 and divided by 1 or itself only
+
+num = int(input("Enter a no: "))
+count = 0 
+
+if num <= 1:
+    print("No. is not a Prime number")
+
+for i in range(1, num + 1):
+    if num%i==0:
+        count += 1    
+
+if count == 2:
+    print(f"{num} is a Prime number")
+else:
+    print(f"{num} is not a Prime number")
+    
+
+
+
+
